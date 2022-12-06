9bd320c98eef8353a8368fc79cfd0345229d2072
Lucas Moura <93lucasribeiro@gmail.com>
2022-12-06 13:38:00 -0300

    adicionando configuração para conexão com o banco de dados

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
-  database: 'TRYBE_FUTEBOL_CLUBE',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3002,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
@@ -14,4 +14,8 @@ const config: Options = {
   logging: false,
 }
 
-module.exports = config;
+module.exports = {
+  development: config,
+  test: config,
+  production: config,
+};
